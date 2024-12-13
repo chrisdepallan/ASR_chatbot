967ab5e7926194c7bd6ea899b5f4619d9a7d0baa
Chris Dennis Pallan <70682683+chrisdepallan@users.noreply.github.com>
2024-12-13 09:21:37 +0530

    Update main.py

--- api/main.py
+++ api/main.py
@@ -10,8 +10,8 @@ from fastapi.middleware.httpsredirect import HTTPSRedirectMiddleware
 app = FastAPI()
 
 # Add middlewares
-# app.add_middleware(TrustedHostMiddleware, allowed_hosts=["ust.chrisdepallan.com"])
-# app.add_middleware(HTTPSRedirectMiddleware)
+app.add_middleware(TrustedHostMiddleware, allowed_hosts=["ust.chrisdepallan.com"])
+app.add_middleware(HTTPSRedirectMiddleware)
 
 templates = Jinja2Templates(directory=os.path.join(os.path.dirname(__file__), "templates"))
 
